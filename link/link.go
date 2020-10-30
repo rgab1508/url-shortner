fdd5b5432d44e5ce4c46f21b3dc30de08b1a66de
Gabriel Rajendran <rgabriel1508@gmail.com>
2020-10-30 16:59:57 +0530

    added NewLink route

--- link/link.go
+++ link/link.go
@@ -2,12 +2,25 @@ package link
 
 import (
   "github.com/gofiber/fiber"
+  "github.com/rgab1508/url-shortner/utils"
 )
+
+type Link struct {
+  Slug string `json:"slug"`
+  Url string  `json:"url"`
+}
 
 func GetLink(c *fiber.Ctx){
   c.Send("This is a Link")
 }
 
 func NewLink(c *fiber.Ctx){
-  c.Send("New Link")
+  //var link Link
+  link := new(Link)
+  if err := c.BodyParser(link); err != nil {
+    c.Status(500).Send(err)
+    return
+  }
+  link.Slug = utils.RandomSlug(6)
+    c.JSON(link)
 }
--- main.go
+++ main.go
@@ -7,14 +7,14 @@ import (
 )
 
 
-func SetupRoutes(app *fiber.App){
+func setupRoutes(app *fiber.App){
   app.Get("/api/v1/:id", link.GetLink)
   app.Post("/api/v1/new", link.NewLink)
 }
 
 func main(){
   app := fiber.New()
-  SetupRoutes(app)
+  setupRoutes(app)
   fmt.Println("Server Running......")
 	app.Listen(3000)
 }
