7a9da3e1a838b0abf6b7b724a55161c1ff1250f4
Gabriel Rajendran <rgabriel1508@gmail.com>
2020-11-01 11:13:42 +0530

    added Save to firebase

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/rgab1508/url-shortner
 
 go 1.15
 
-require github.com/gofiber/fiber v1.14.6
+require (
+	cloud.google.com/go/firestore v1.3.0 // indirect
+	firebase.google.com/go v3.13.0+incompatible
+	github.com/gofiber/fiber v1.14.6
+	google.golang.org/api v0.34.0
+)
--- link/link.go
+++ link/link.go
@@ -1,8 +1,15 @@
 package link
 
 import (
+  "os"
+  "log"
+  "context"
+
   "github.com/gofiber/fiber"
   "github.com/rgab1508/url-shortner/utils"
+
+  firebase "firebase.google.com/go"
+  "google.golang.org/api/option"
 )
 
 type Link struct {
@@ -10,17 +17,44 @@ type Link struct {
   Url string  `json:"url"`
 }
 
+func (l *Link) Save(){
+  ctx := context.Background()
+  sa := option.WithCredentialsFile(os.Getenv("PATH_TO_SERVICE_FILE"))
+  conf := &firebase.Config{ProjectID: os.Getenv("PROJECT_ID")}
+  app, err := firebase.NewApp(ctx, conf, sa)
+  if err != nil {
+    log.Fatalln(err)
+  }
+
+  client, err := app.Firestore(ctx)
+  if err != nil {
+    log.Fatalln(err)
+  }
+  defer client.Close()
+
+  _, _, err = client.Collection("links").Add(ctx, map[string]interface{}{
+    "slug": l.Slug,
+    "url":  l.Url,
+  })
+  if err != nil {
+    log.Fatalf("Failed adding Link : %v", err)
+  }
+}
+
+/* ROUTES */
+
 func GetLink(c *fiber.Ctx){
-  c.Send("This is a Link")
+  id := c.Params("id")
+  c.Send(id)
 }
 
 func NewLink(c *fiber.Ctx){
-  //var link Link
   link := new(Link)
   if err := c.BodyParser(link); err != nil {
     c.Status(500).Send(err)
     return
   }
   link.Slug = utils.RandomSlug(6)
-    c.JSON(link)
+  link.Save()
+  c.JSON(link)
 }
